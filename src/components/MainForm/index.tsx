8b18faa8f5af606a1e2686fc35c3ba2d788590ba
Alexandre Cardoso <cardoal01@gmail.com>
2025-05-27 13:09:23 -0300

    Implement form submission handling in MainForm; manage task name state and update input binding

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,25 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cicles } from '../Cicles';
 import { DefaultButton } from '../DefaultButton/Index';
 import { DefaultInput } from '../DefaultInput/Index';
+import { FormEvent, useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+  function handleCreateNewTask(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log('pausei o submit', taskName);
+  }
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='Task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
         />
       </div>
       <div className='formRow'>
