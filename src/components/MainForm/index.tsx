aa54e81a487919397f60ee4cfd813e5605a331eb
Alexandre Cardoso <cardoal01@gmail.com>
2025-06-03 16:20:24 -0300

    Refactor task management in MainForm to use useReducer; implement task start and interrupt actions

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,16 +1,16 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Cycles } from '../Cycles';
-import { DefaultButton } from '../DefaultButton/Index';
-import { DefaultInput } from '../DefaultInput/Index';
-import { FormEvent, useRef } from 'react';
-import { TaskModel } from '../../models/TaskModel';
-import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
-import { getNextCycle } from '../../utils/getNextCycle';
-import { getNextCycleType } from '../../utils/getNextCycleType';
-import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaultButton } from "../DefaultButton/Index";
+import { DefaultInput } from "../DefaultInput/Index";
+import { FormEvent, useRef } from "react";
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   // const [taskName, setTaskName] = useState('');
   const taskNameInput = useRef<HTMLInputElement>(null);
 
@@ -26,7 +26,7 @@ export function MainForm() {
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite o nome da tarefa');
+      alert("Digite o nome da tarefa");
       return;
     }
 
@@ -40,78 +40,53 @@ export function MainForm() {
       type: nextCycleType,
     };
 
-    const secondsRemaining = newTask.duration * 60;
-
-    setState((prevState) => {
-      return {
-        ...prevState,
-        config: { ...prevState.config },
-        activeTask: newTask,
-        currentCycle: nextCycle,
-        secondsRemaining,
-        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
   }
 
   function handleInterruptTask() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        activeTask: null,
-        secondsRemaining: 0,
-        formattedSecondsRemaining: '00:00',
-        tasks: prevState.tasks.map((task) => {
-          if (prevState.activeTask && prevState.activeTask.id === task.id) {
-            return { ...task, interruptDate: Date.now() };
-          }
-          return task;
-        }),
-      };
-    });
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
-    <form onSubmit={handleCreateNewTask} className='form' action=''>
-      <div className='formRow'>
+    <form onSubmit={handleCreateNewTask} className="form" action="">
+      <div className="formRow">
         <DefaultInput
-          labelText='Task'
-          id='meuInput'
-          type='text'
-          placeholder='Digite algo'
+          labelText="Task"
+          id="meuInput"
+          type="text"
+          placeholder="Digite algo"
           // value={taskName}
           // onChange={(e) => setTaskName(e.target.value)}
           ref={taskNameInput}
           disabled={!!state.activeTask}
         />
       </div>
-      <div className='formRow'>
+      <div className="formRow">
         <p>Próximo intervalo é de 25min</p>
       </div>
       {state.currentCycle > 0 && (
-        <div className='formRow'>
+        <div className="formRow">
           <Cycles />
         </div>
       )}
-      <div className='formRow'>
+      <div className="formRow">
         {!state.activeTask ? (
           <DefaultButton
-            aria-label='Iniciar nova tarefa'
-            title='Iniciar nova tarefa'
-            type='submit'
+            aria-label="Iniciar nova tarefa"
+            title="Iniciar nova tarefa"
+            type="submit"
             icon={<PlayCircleIcon />}
-            key='submit'
+            key="submit"
           />
         ) : (
           <DefaultButton
-            aria-label='Interromper tarefa'
-            title='Interromper tarefa'
-            type='button'
-            color='red'
+            aria-label="Interromper tarefa"
+            title="Interromper tarefa"
+            type="button"
+            color="red"
             icon={<StopCircleIcon />}
             onClick={handleInterruptTask}
-            key='button'
+            key="button"
           />
         )}
       </div>
