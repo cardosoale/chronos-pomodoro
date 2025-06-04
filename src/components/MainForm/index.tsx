e2e9ed3d8c6c84ad575d86a4a5ee9147052fcd7d
Alexandre Cardoso <cardoal01@gmail.com>
2025-06-04 13:11:55 -0300

    Add Tips component to MainForm for displaying task-related tips; refactor interval message

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+import { Tips } from "../Tips";
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -62,7 +63,7 @@ export function MainForm() {
         />
       </div>
       <div className="formRow">
-        <p>Próximo intervalo é de 25min</p>
+        <Tips />
       </div>
       {state.currentCycle > 0 && (
         <div className="formRow">
---
+++ src/workers/TimeWorkManeger.ts
@@ -0,0 +1,29 @@
+let instance: TimeWorkManager | null;
+
+export class TimeWorkManager {
+  private worker: Worker;
+
+  private constructor() {
+    this.worker = new Worker(new URL("./timeWorker.js", import.meta.url));
+  }
+
+  static getInstance() {
+    if (!instance) {
+      instance = new TimeWorkManager();
+    }
+    return instance;
+  }
+
+  postMessage(message: any) {
+    this.worker.postMessage(message);
+  }
+
+  onmessage(cb: (e: MessageEvent) => void) {
+    this.worker.onmessage = cb;
+  }
+
+  terminate() {
+    this.worker.terminate();
+    instance = null;
+  }
+}
