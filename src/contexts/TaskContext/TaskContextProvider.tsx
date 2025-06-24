9adf4bd045a21aa819aa69be22ebe25af12a6449
Alexandre Cardoso <cardoal01@gmail.com>
2025-06-24 12:31:00 -0300

    feat: enhance TaskContextProvider to initialize state from localStorage and update on state changes

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,13 +5,27 @@ import { taskReducer } from "./taskReducer";
 import { TimeWorkManager } from "../../workers/TimeWorkManager";
 import { TaskActionTypes } from "./taskActions";
 import { loadBeep } from "../../utils/loadBeep";
+import { TaskStateModel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+    const storageState = localStorage.getItem("state");
+
+    if (!storageState) return initialTaskState;
+
+    const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+
+    return {
+      ...parsedStorageState,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: "00:00",
+    };
+  });
 
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
@@ -38,6 +52,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   });
 
   useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+
     if (!state.activeTask) {
       worker.terminate();
     }
